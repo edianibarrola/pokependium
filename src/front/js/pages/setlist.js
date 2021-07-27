c4295f825b2353072b3f50cec199faf5ae98f736
edianibarrola <edianibarrola@gmail.com>
2021-07-27 22:22:09 +0000

    set list and icon fetch working

--- src/front/js/pages/setlist.js
+++ src/front/js/pages/setlist.js
@@ -20,6 +20,7 @@ export const SetList = props => {
 						<ListGroup.Item action href="#link1">
 							Link 1
 						</ListGroup.Item>
+
 						<ListGroup.Item action href="#link2">
 							Link 2
 						</ListGroup.Item>
@@ -28,7 +29,26 @@ export const SetList = props => {
 				<Col sm={8}>
 					<Tab.Content>
 						<Tab.Pane eventKey="#link1">
-							<h1>h1</h1>
+							{store.setList.data ? (
+								store.setList.data.map((item, i) => {
+									return (
+										<div key={i} className="row">
+											<div className="col-1">
+												<img
+													style={{ height: "2em", "padding-left": "2em" }}
+													src={item.images.symbol}
+													alt=""
+												/>
+											</div>
+											<div className="col-8 text-center">
+												<h1 style={{ "padding-left": "2em" }}> {item.name} </h1>
+											</div>
+										</div>
+									);
+								})
+							) : (
+								<li>hello</li>
+							)}
 						</Tab.Pane>
 						<Tab.Pane eventKey="#link2">
 							<h1>h1 2</h1>
