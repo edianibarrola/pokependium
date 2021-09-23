af2cba782bb05656fe4d600552a107b99e9da2b0
edianibarrola <edianibarrola@gmail.com>
2021-09-23 04:14:52 +0000

    linking front to back for editing owned cards

--- src/front/js/component/cardlist.js
+++ src/front/js/component/cardlist.js
@@ -46,7 +46,14 @@ export const CardList = props => {
 									<div>Standard</div>
 									{store.owned.find(card => card.card_id == item.id) ? (
 										store.owned.find(card => card.card_id == item.id).standard_art == true ? (
-											<input type="checkbox" value="true" checked />
+											<input
+												type="checkbox"
+												value="true"
+												checked
+												onClick={() => {
+													console.log(store.owned.find(card => card.card_id == item.id));
+												}}
+											/>
 										) : (
 											<input type="checkbox" value="false" />
 										)
